0229acd9fb0a6c333b08eedbd3d652e57a896c8c
Scot Curry <scotcurry4@gmail.com>
2024-06-01 19:34:22 -0400

    Working on log correlation.

--- src/main/java/org/curryware/currywarejava/CurrywareJavaApplication.java
+++ src/main/java/org/curryware/currywarejava/CurrywareJavaApplication.java
@@ -1,13 +1,23 @@
 package org.curryware.currywarejava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrywareJavaApplication {
+public class CurrywareJavaApplication implements ApplicationRunner {
 
+    private static final Logger logger = LoggerFactory.getLogger(CurrywareJavaApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(CurrywareJavaApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        logger.debug("Debug - Curryware application started");
+    }
 }
--- src/main/java/org/curryware/currywarejava/controller/FantasyTeamController.java
+++ src/main/java/org/curryware/currywarejava/controller/FantasyTeamController.java
@@ -1,8 +1,8 @@
 package org.curryware.currywarejava.controller;
 
 import com.google.gson.Gson;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.curryware.currywarejava.models.TeamInfo;
 import org.curryware.currywarejava.services.TeamInfoService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,7 @@ import java.util.List;
 @RequestMapping("teaminfo")
 public class FantasyTeamController {
 
-    private static final Logger logger = LogManager.getLogger(FantasyTeamController.class);
+    private static final Logger logger = LoggerFactory.getLogger(FantasyTeamController.class);
     private final TeamInfoService teamInfoService;
 
     public FantasyTeamController(TeamInfoService teamInfoService) {
@@ -27,13 +27,13 @@ public class FantasyTeamController {
     @GetMapping(value = "/getteams", produces = "application/json")
     public @ResponseBody String getTeamInfo() {
 
-        String jsonString;
         logger.info("Getting team information");
         Iterable<TeamInfo> teamInfoList = teamInfoService.findAll();
         List<TeamInfo> teamInfo = new ArrayList<>();
         teamInfoList.forEach(teamInfo::add);
-        logger.info("Found {} teams", teamInfo.size());
-        jsonString = new Gson().toJson(teamInfo);
+        logger.error("Found {} teams", teamInfo.size());
+        String jsonString = new Gson().toJson(teamInfo);
+        teamInfo.clear();
 
         return jsonString;
     }
